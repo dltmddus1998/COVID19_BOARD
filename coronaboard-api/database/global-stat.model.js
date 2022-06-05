f8d17f870c36df7598e136f53a2996523ea6361c
dltmddus1998 <dltmddus1998@naver.com>
2022-06-06 00:21:19 +0900

    ADD: Making API

--- coronaboard-api/database/global-stat.model.js
+++ coronaboard-api/database/global-stat.model.js
@@ -10,7 +10,55 @@ module.exports = sequelize => {
                 allowNull: false,
                 primaryKey: true,
             },
-            
-        }
-    )
-}
+            cc: { // country code
+                type: DataTypes.CHAR(2),
+                allowNull: false,
+            },
+            date: {
+                type: DataTypes.DATEONLY,
+                allowNull: false,
+            },
+            confirmed: { // 확진자 수
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            death: { // 사망자 수
+                type: DataTypes.INTEGER,
+                allowNull: true,
+            },
+            released: { // 완치자 수
+                type: DataTypes.INTEGER,
+                allowNull: true,
+            },
+            tested: { // 총 검사자 수
+                type: DataTypes.INTEGER,
+                allowNull: true,
+            },
+            testing: { // 검사중 수
+                type: DataTypes.INTEGER,
+                allowNull: true,
+            },
+            negative: { // 결과 음성 수
+                type: DataTypes.INTEGER,
+                allowNull: true,
+            },
+        },
+        {
+            sequelize,  // 시퀄라이즈 인스턴스
+            tableName: 'GlobalStat',    // 데이터베이스에서 테이블명
+            indexes: [  // 테이블 인덱스
+                {
+                    name: 'PRIMARY',
+                    unique: true,
+                    fields: [{ name: 'id' }],
+                },
+                {
+                    name: 'ccWithDate',
+                    unique: true,
+                    fields: [{ name: 'cc' }, { name: 'date' }],
+                },
+            ],
+            timestamps: false, // 타임스탬프 속성 자동 생성 x
+        },
+    );
+};
--- coronaboard-api/index.js
+++ coronaboard-api/index.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { sequelize } = require('./database');
+const globalStatController = require('./controller/global-stat.controller.js');
 
-const app = express(); // 익스프레스 인스턴스 생성
+async function launchServer() {
+    const app = express(); // 익스프레스 인스턴스 생성
+    // Content-Type의 application/json인 HTTP 요청의 바디를 파싱할 수 있도록 설정
+    app.use(bodyParser.json());
 
-// Content-Type의 application/json인 HTTP 요청의 바디를 파싱할 수 있도록 설정
-app.use(bodyParser.json());
+    app.get('/', (req, res) => {
+        res.json({ message: 'Hello CoronaBoard!' });
+    });
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello CoronaBoard!' });
-});
+    try {
+        await sequelize.sync(); // 2.
+        console.log('Database is ready!');
+    } catch(error) {
+        console.log('Unable to connect to the database:');
+        console.log(error);
+        process.exit(1);
+    }
 
-const port = process.env.PORT || 8080; // 포트 기본값을 8080으로 지정
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+    app.get('/global-stats', globalStatController.getAll);
+    app.post('/global-stats', globalStatController.insertOrUpdate);
+    app.delete('/global-stats', globalStatController.remove);
+
+    const port = process.env.PORT || 8080; // 포트 기본값을 8080으로 지정
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}
+
+launchServer();
+
+
